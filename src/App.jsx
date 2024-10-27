24a7e1136be53057a0751c4e757727799abb7092
mohamadAlrimi <mohamadrimi12345@gmail.com>
2024-10-27 15:34:30 +0300

    Outputting Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     expectedReturn: 6,
     duration: 10,
   });
+  const inputIsValid = userInput.duration >= 1;
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -21,7 +22,8 @@ function App() {
     <>
     <Header />
     <UserInput  userInput={userInput} onChange ={handleChange}/>
-    <Results input={userInput}/>
+    {!inputIsValid && (<p className="center">Please enter a duration greater than zero </p>)}
+       { inputIsValid &&<Results input={userInput}/>} 
     </>
   )
 }
